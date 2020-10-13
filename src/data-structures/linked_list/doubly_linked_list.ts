b56021728c3ab4a15ff849232b44c2b351894d21
Albert <bert@berts-MacBook-Pro.local>
2020-10-13 20:06:33 +0100

    implement doubly linked list

--- src/data-structures/linked_list/doubly_linked_list.ts
+++ src/data-structures/linked_list/doubly_linked_list.ts
@@ -1,13 +1,8 @@
-interface ListNode {
-  value: any;
-  next: ListNode | null;
-  prev?: ListNode | null;
-}
-
-class LinkedList {
+class DoublyLinkedList {
   constructor(value: any) {
     this.head = {
       value: value,
+      prev: null,
       next: null,
     };
     this.tail = this.head;
@@ -20,8 +15,11 @@ class LinkedList {
 
   //add to back/end
   append(value: any) {
+    const prevNode: ListNode = this.head;
+
     let newNode: ListNode = {
       value: value,
+      prev: prevNode,
       next: null,
     };
 
@@ -35,11 +33,15 @@ class LinkedList {
 
   //add to front
   prepend(value: any) {
-    const newNode = {
+    const prevHead = this.head;
+
+    const newNode: ListNode = {
       value: value,
+      prev: null,
       next: null,
     };
 
+    prevHead.prev = newNode;
     newNode.next = this.head;
     this.head = newNode;
 
@@ -57,13 +59,17 @@ class LinkedList {
 
     const newNode: ListNode = {
       value: val,
+      prev: null,
       next: null,
     };
 
     const prevNode: ListNode = this.traverseToIndex(index - 1);
-    const nextNode: ListNode = prevNode.next;
+    const currentNode: ListNode = prevNode.next;
+
+    newNode.prev = prevNode;
     prevNode.next = newNode;
-    newNode.next = nextNode;
+    currentNode.prev = newNode;
+    newNode.next = currentNode;
 
     this.length++;
     return this.printList();
@@ -78,6 +84,7 @@ class LinkedList {
     const currNode: ListNode = prevNode.next;
     const nextNode: ListNode = currNode.next;
 
+    nextNode.prev = prevNode;
     prevNode.next = nextNode;
 
     this.length--;
@@ -110,17 +117,15 @@ class LinkedList {
   }
 }
 
-const linkedList1 = new LinkedList(10);
-linkedList1.append(5);
-linkedList1.append(16);
-linkedList1.prepend(8);
-linkedList1.insert(2, 99);
-linkedList1.insert(3, 88);
-linkedList1.remove(2);
-console.log(linkedList1.printList());
+const doublyLinkedList = new DoublyLinkedList(10);
+doublyLinkedList.append(5);
+doublyLinkedList.prepend(20);
+doublyLinkedList.prepend(70);
+doublyLinkedList.insert(2, 99);
+doublyLinkedList.insert(3, 200);
 
-// function gcdOfStrings(str1: string, str2: string): string {
-//   if (str1.includes(str2 + str2)) {
+console.log(doublyLinkedList.printList());
 
-//   }
-// }
+doublyLinkedList.remove(2);
+
+console.log(doublyLinkedList.printList());
