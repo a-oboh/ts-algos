b6d794ab6f26d29a1c8b45e75e090b2e5817722a
Albert Oboh <albertoboh7@gmail.com>
2020-11-03 00:22:14 +0100

    implement quick sort

---
+++ src/algorithms/sorting/quick-sort/quick_sort.ts
@@ -0,0 +1,42 @@
+const nums = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array: any[], left, right) {
+  let pivot: any;
+  let partitionIndex: number;
+
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+
+    //sort left and right
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+
+function partition(array, pivot, left, right) {
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(nums, 0, nums.length - 1);
+console.log(nums);
+
+export {};
