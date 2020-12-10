81a1d19f651d7e3700f408c992fc695b20aa69af
Albert Oboh <albertoboh7@gmail.com>
2020-12-10 22:10:46 +0100

    add link to trapping rain water

--- src/playground/Arrays/trapping_rain_water.ts
+++ src/playground/Arrays/trapping_rain_water.ts
@@ -1,3 +1,5 @@
+// question link: https://leetcode.com/problems/trapping-rain-water/
+
 // Given n non - negative integers representing an elevation map where
 // the width of each bar is 1, compute how much water it can trap after raining.
 
