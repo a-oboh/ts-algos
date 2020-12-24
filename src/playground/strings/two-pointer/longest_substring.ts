21a70d977377fe3179d6db6d3b417dc88232c3a8
Albert Oboh <albertoboh7@gmail.com>
2020-12-24 11:57:30 +0100

    start M_N reversal linked list

--- src/playground/strings/two-pointer/longest_substring.ts
+++ src/playground/strings/two-pointer/longest_substring.ts
@@ -1,3 +1,6 @@
+//Question URL: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+//two pointers
 function lengthOfLongestSubstring(s: string): number {
   if (s == "") {
     return 0;
@@ -49,7 +52,52 @@ function lengthOfLongestSubstring(s: string): number {
   return max;
 }
 
-const s = " ";
-lengthOfLongestSubstring(s);
+function longestSubstring(s: string) {
+  if (s == "") {
+    return 0;
+  } else if (s.length == 1) {
+    return 1;
+  }
+
+  let longest = 0;
+
+  let p1 = 0,
+    p2 = 0;
+
+  // let seenChars = {};
+
+  let chars = new Map<string, number>();
+  let curr = 0;
+
+  while (p1 < s.length) {
+    if (!chars.has(s[p2])) {
+      chars.set(s[p2], p2);
+      curr++;
+      longest = Math.max(curr, longest);
+      
+      if (p2 < s.length) {
+        p2++;
+      } else {
+        p1++
+        continue;
+      }
+    } else {
+      curr = 0;
+      p1 = chars.get(s[p2]) + 1;
+      p2 = p1;
+      chars.clear();
+      
+    }
+
+    console.log(chars);
+  }
+
+  return longest;
+}
+
+const s = "pwwkew";
+
+// lengthOfLongestSubstring(s);
+console.log(longestSubstring(s));
 
 export {};
