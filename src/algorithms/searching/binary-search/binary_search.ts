b2cdca5d295fc2b5dff9ea3cd3d8b92fee7d19b1
Albert Oboh <albertoboh7@gmail.com>
2021-02-10 12:40:20 +0100

    minor changes

--- src/algorithms/searching/binary-search/binary_search.ts
+++ src/algorithms/searching/binary-search/binary_search.ts
@@ -1,19 +1,25 @@
 // good for sorted values
 function binarySearch(arr: any[], left: number, right: number, k: any) {
-  let mid = (right + left) / 2;
-  mid = Math.round(mid);
+  // let left = 0;
+  // let right = arr.length - 1;
 
-  if (arr[mid] < k) {
-    return binarySearch(arr, mid + 1, right, k);
-  } else if (arr[mid] > k) {
-    return binarySearch(arr, left, mid - 1, k);
-  } else {
-    return mid;
+  const mid = Math.round((right + left) / 2);
+
+  if (left <= right) {
+    if (arr[mid] < k) {
+      return binarySearch(arr, mid + 1, right, k);
+    } else if (arr[mid] > k) {
+      return binarySearch(arr, left, mid - 1, k);
+    } else if (arr[mid] === k) {
+      return mid;
+    }
   }
+
+  return -1;
 }
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const nums = [1, 2, 3, 4, 6, 7, 8];
 
-console.log(binarySearch(nums, 0, nums.length - 1, 7));
+// console.log(binarySearch(nums, 0, nums.length - 1, 5));
 
 export { binarySearch };
